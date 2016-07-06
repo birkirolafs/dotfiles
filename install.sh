f8a1aff9eb39fc4962dd2e11b6a4082c635997f0
Birkir Ólafsson <birkir@trappa.is>
2016-07-06 00:14:55 +0000

    herr director

--- install.sh
+++ install.sh
@@ -1,9 +1,34 @@
 #!/usr/bin/env bash
 
-echo "Initializing dotfiles"
+stty -echo         # turn-off echoing.
+read -s -p Password: pwd
+stty echo		   # turn-on  echoing
+
+echo -e "\n\nInitializing dotfiles"
 
 echo "Initializing submodule(s)"
 git submodule update --init --recursive
 
-echo "Initializing submodule(s) Completed !"
-echo "Initializing dotfiles     Completed !"
+source install/link.sh
+
+if [ "$(uname)" == "Linux" ]; then
+	echo -e "\n\nRunning on Linux"
+
+	# sudo install binaries with yum
+	echo -e $pwd | sudo -S ./install/yum.sh
+
+	source install/nvm.sh
+
+	source install/npm.sh
+fi
+
+if [ "$(uname)" == "Darwin" ]; then
+	echo -e "\n\nRunning on OSX"
+
+	source install/brew.sh
+fi
+
+
+unset pwd
+
+echo "done"
