5fee6fe73b5c0447772f1dcedfa651eed4ed0734
Birkir Ólafsson <birkir@trappa.is>
2016-12-08 21:19:43 +0000

    adding tmux helper

--- install/yum.sh
+++ install/yum.sh
@@ -3,9 +3,15 @@
 # taken from https://github.com/neovim/neovim/wiki/Installing-Neovim
 echo -e "\n\nInstalling essential binaries with yum"
 echo "=============================="
-yum -y -q install epel-release
-curl -s -o /etc/yum.repos.d/dperson-neovim-epel-7.repo https://copr.fedorainfracloud.org/coprs/dperson/neovim/repo/epel-7/dperson-neovim-epel-7.repo 
-yum -y -q install neovim
 
-yum -y -q install git tree htop ack
+echo -e "\n\nAdding repositories for yum"
+curl -s -o /etc/yum.repos.d/dperson-neovim-epel-7.repo https://copr.fedorainfracloud.org/coprs/dperson/neovim/repo/epel-7/dperson-neovim-epel-7.repo 
+
+yum -y -q install epel-release
+yum -y -q install neovim
+yum -y -q install ack
+yum -y -q install git 
+yum -y -q install tree
+yum -y -q install htop
 yum -y -q install docker
+yum -y -q install tmux
